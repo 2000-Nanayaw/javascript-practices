a13ec678ed389a4da5f1c653f7ac273d0d7726ee
2000-Nanayaw <samunanaadomah@gmail.com>
2022-05-31 15:21:46 -0700

    javascript-practices

---
+++ js1.js
@@ -0,0 +1,63 @@
+// class dogs{
+// 	constructor(local,bulldog,age){
+// 		this.local=local;
+// 		this.bulldog="bulldog";
+// 		this.age=age;
+// 		this.taste="nice"
+		
+// 	}
+// }
+// let types= new dogs("fat","slim",34)
+// console.log("this"+types['local']+types.bulldog)
+
+
+
+// function letTest(){
+// 	let x
+// }
+// let x= 29;
+// alert(x)
+// {
+// 	alert("adomah")
+// }
+
+// let x= function(a,b){
+//     let x=  a * b
+// }
+// console.log(6*7)
+// alert(9)
+
+// let name= function(a,b){
+// 	return a * b;
+// }
+// console.log(name)
+
+// let x= function(a ,b){
+// 	return a * b
+// }
+// console.log(x(90,8))
+
+
+// function tePa(one,two){
+// 	let fullname= one + "" + two
+// 	document.write(fullname)
+// }
+// tePa("happy", "sad")
+
+// function getAge(first,second){
+// 	return first + "" + second
+
+// }
+// let finname=getAge("kumase","accra")
+// document.write(finname)
+
+function getAge(age){
+	let namE = "jsar";
+	document.write(` ${age}`)
+}
+
+
+function Date(years){
+	let name = "christmas";
+	document.write(`my name is ${name} and i am ${years}`)
+}
